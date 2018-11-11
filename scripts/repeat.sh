51ce37aa13e078bbe44d90b10c6f1d5f056cbec7
drdhaval2785 <drdhaval2785@gmail.com>
2018-11-11 23:24:36 +0530

    Shivadatta renamed

--- scripts/repeat.sh
+++ scripts/repeat.sh
@@ -1,8 +1,8 @@
 # Usage - sh repeat.sh "message for commit"
 python preprocess.py
 python manualCorrection.py
-cp ../derivedFiles/cataloguev006.tsv ../PanditShivadattaShukla.tsv
+cp ../derivedFiles/cataloguev006.tsv ../ShivadattaShuklaPrakashanandanatha.tsv
 git add ../derivedFiles/
-git add ../PanditShivadattaShukla.tsv
+git add ../ShivadattaShuklaPrakashanandanatha.tsv
 git commit -m "$1"
 git push origin master
